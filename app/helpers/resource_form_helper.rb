427c1d2aa90e170cd6e4bc8c729e82c447154f0c
holysugar <holysugar@gmail.com>
2012-04-10 22:53:44 +0900

    semantic common form !

---
+++ app/helpers/resource_form_helper.rb
@@ -0,0 +1,8 @@
+module ResourceFormHelper
+  def resource_form(resource, options = {}, &block)
+    render 'semantic_form', options.reverse_merge(:resource => resource, :url => nil)
+  end
+end
+
+ApplicationHelper.send(:include, ResourceFormHelper)
+
