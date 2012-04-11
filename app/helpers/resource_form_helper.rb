da2f78aafc3c1590baaec8fe9da1359d9c8a6532
holysugar <holysugar@gmail.com>
2012-04-11 11:36:15 +0900

    Add builder option in resource_form

--- app/helpers/resource_form_helper.rb
+++ app/helpers/resource_form_helper.rb
@@ -1,6 +1,6 @@
 module ResourceFormHelper
   def resource_form(resource, options = {}, &block)
-    render 'semantic_form', options.reverse_merge(:resource => resource, :url => nil)
+    render 'semantic_form', options.reverse_merge(:resource => resource, :url => nil, :builder => nil)
   end
 end
 
