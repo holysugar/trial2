7a5f938f328ed40d6a71419022b5145af00701d3
holysugar <holysugar@gmail.com>
2012-04-18 13:31:14 +0900

    add item factory

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,10 +2,10 @@
 
 FactoryGirl.define do
   factory :item do
-    name "MyString"
-    price 1
-    description "MyText"
-    kind 1
-    enabled false
+    name        { Forgery::Basic.text }
+    price       { Random.rand(20000) }
+    description { Forgery::Basic.text }
+    kind        { Random.rand(3) + 1 }
+    enabled     { [true, false].sample }
   end
 end
